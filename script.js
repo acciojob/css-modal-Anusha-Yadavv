ba3d170ebe68c88f22584ad877705c1767b8afb6
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2023-11-21 23:55:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+
+document.addEventListener('DOMContentLoaded', function () {
+    var modal = document.getElementById('myModal');
+    var btn = document.getElementById('openModal');
+    var closeButton = document.getElementsByClassName('close-modal')[0];
+
+    btn.onclick = function () {
+        modal.style.display = 'block';
+    }
+
+    window.onclick = function (event) {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    }
+
+    closeButton.onclick = function () {
+        modal.style.display = 'none';
+    }
+});
