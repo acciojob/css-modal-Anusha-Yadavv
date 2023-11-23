947d41c10ac1e42d7b40dffffbf3e0bdfa61e76f
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2023-11-23 15:28:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 //your JS code here. If required.
+function onOpenModal() {
+	 var modal = document.getElementById('myModal');
+     var btn = document.getElementById('openModal');
+	  modal.style.display = 'block';
+}
+
+// Function to close the modal
+function onCloseModal() {
+	var closeBtn = document.querySelector('.close-modal');
+	var modal = document.getElementById('myModal');
+	modal.style.display = 'none';
+}
+
+
+
+
+
 	   
        
     
